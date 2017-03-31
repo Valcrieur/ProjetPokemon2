6ee6b25e3e139a6bab024dfe2283f190f669159d
Valen <valentin.vendi@gmail.com>
2017-03-31 10:39:40 +0200

    Remplacement librairie MySqli par PDO

--- www/index.php
+++ www/index.php
@@ -1,29 +1,53 @@
 <?php
-try {
-    $dbh = new PDO('mysql:host=localhost;dbname=pokemon', 'root', 'root');
-    $dbh->setAttribute(PDO::ATTR_CASE, PDO::CASE_LOWER);
-    $dbh->setAttribute(PDO::ATTR_ERRMODE , PDO::ERRMODE_EXCEPTION);
-    $dbh->exec("SET NAMES utf8");
-} catch (PDOException $e) {
-    die ('Une erreur MySQL est arrivée: ' . $e->getMessage());
+try
+{
+    $bdd = new PDO('mysql:host=localhost;dbname=pokemon;charset=utf8', 'root', 'root');
 }
-$res = $dbh->query('Select * from pokemon');
+catch (Exception $e)
+{
+    die('Erreur : ' . $e->getMessage());
+}
+
+$reponse = $bdd->query('SELECT * FROM pokemon');
+
 ?>
 <!doctype html>
-<html lang="fr">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Pokemon</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </head>
 <body>
-<?php
-while ($row = $res->fetch()) {
-    echo " nom = " . $row['nom'] . ", type = " . $row['type'] . "<br>";
-}
-?>
+<table class="table">
+    <thead>
+    <tr>
+        <th>Nom</th>
+        <th>Type</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php
+    while ($row = $reponse->fetch()) {
+        ?>
+        <tr>
+            <td><?php echo $row['nom']; ?></td>
+            <td><?php echo $row['type']; ?></td>
+        </tr>
+        <?php
+    }
+    ?>
+    </tbody>
+</table>
 </body>
 </html>
-
